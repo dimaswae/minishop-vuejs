73a315ed8cdee66fb7f30eae84e73fd226bdd228
Dimas Rizqia Hidayat <dimaswae012@gmail.com>
2026-02-02 13:58:40 +0700

    deploy gh-page

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: "./minishop-vuejs/",
+  base: '/minishop-vuejs/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
